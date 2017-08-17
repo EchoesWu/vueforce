0fdd66c2af99bb5a48d1306017b019255ff03374
Echoes <echoes.wu@outlook.com>
2017-08-17 16:11:04 +0800

    [build] 1.5.5

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,6 @@
-import Vue from 'vue'
 import Vuex from 'vuex'
 import loading from './modules/loading'
 import slds from './modules/slds'
-
-Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
